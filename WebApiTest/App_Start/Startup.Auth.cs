2079913d1338e7c012f3db98291b0c04150c2374
Oleg Shatin 11-501 <mongoose0911@gmail.com>
2017-03-10 13:59:56 +0300

    init

--- WebApiTest/App_Start/Startup.Auth.cs
+++ WebApiTest/App_Start/Startup.Auth.cs
@@ -17,53 +17,70 @@ namespace WebApiTest
     {
         public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
 
-        public static string PublicClientId { get; private set; }
+         public static string PublicClientId { get; private set; }
+        
+         // Дополнительные сведения о настройке аутентификации см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301864
+         public void ConfigureAuth(IAppBuilder app)
+         {
+             // Настройка контекста базы данных и диспетчера пользователей для использования одного экземпляра на запрос
+             app.CreatePerOwinContext(ApplicationDbContext.Create);
+             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
-        // Дополнительные сведения о настройке аутентификации см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301864
-        public void ConfigureAuth(IAppBuilder app)
+             // Включение использования файла cookie, в котором приложение может хранить информацию для пользователя, выполнившего вход,
+             // и использование файла cookie для временного хранения информации о входах пользователя с помощью стороннего поставщика входа
+             app.UseCookieAuthentication(new CookieAuthenticationOptions());
+             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+             // Настройка приложения для потока обработки на основе OAuth
+             PublicClientId = "self";
+             OAuthOptions = new OAuthAuthorizationServerOptions
+             {
+                 TokenEndpointPath = new PathString("/Token"),
+                 Provider = new ApplicationOAuthProvider(PublicClientId),
+                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                 // В рабочем режиме задайте AllowInsecureHttp = false
+                 AllowInsecureHttp = true
+             };
+
+             // Включение использования приложением маркера-носителя для аутентификации пользователей
+             app.UseOAuthBearerTokens(OAuthOptions);
+
+             // Раскомментируйте приведенные далее строки, чтобы включить вход с помощью сторонних поставщиков входа
+             //app.UseMicrosoftAccountAuthentication(
+             //    clientId: "",
+             //    clientSecret: "");
+
+             //app.UseTwitterAuthentication(
+             //    consumerKey: "",
+             //    consumerSecret: "");
+
+             //app.UseFacebookAuthentication(
+             //    appId: "",
+             //    appSecret: "");
+
+             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+             //{
+             //    ClientId = "",
+             //    ClientSecret = ""
+             //});
+         }
+        //https://github.com/opdahlmann/OwinSample/blob/master/OwinSample/App_Start/Startup.Auth.cs
+        /*static Startup()
         {
-            // Настройка контекста базы данных и диспетчера пользователей для использования одного экземпляра на запрос
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-
-            // Включение использования файла cookie, в котором приложение может хранить информацию для пользователя, выполнившего вход,
-            // и использование файла cookie для временного хранения информации о входах пользователя с помощью стороннего поставщика входа
-            app.UseCookieAuthentication(new CookieAuthenticationOptions());
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Настройка приложения для потока обработки на основе OAuth
-            PublicClientId = "self";
             OAuthOptions = new OAuthAuthorizationServerOptions
             {
-                TokenEndpointPath = new PathString("/Token"),
-                Provider = new ApplicationOAuthProvider(PublicClientId),
-                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
-                // В рабочем режиме задайте AllowInsecureHttp = false
+                TokenEndpointPath = new PathString("/token"),
+                Provider = new MyOAuthProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(2),
                 AllowInsecureHttp = true
             };
-
-            // Включение использования приложением маркера-носителя для аутентификации пользователей
-            app.UseOAuthBearerTokens(OAuthOptions);
-
-            // Раскомментируйте приведенные далее строки, чтобы включить вход с помощью сторонних поставщиков входа
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            //    consumerKey: "",
-            //    consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //    appId: "",
-            //    appSecret: "");
-
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
         }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseOAuthBearerTokens(OAuthOptions);
+        }
+        */
     }
 }
---
+++ WebApiTest/Models/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiTest.Models.Services
+{
+    public class UserService
+    {
+        public static string Hash(string input)
+        {
+            return string.Join("", (new System.Security.Cryptography.SHA1Managed().ComputeHash(System.Text.Encoding.UTF8.GetBytes(input))).Select(x => x.ToString("x2")).ToArray());
+        }
+        public User GetUserByCredentials(string email, string password)
+        {
+            if (email != "email@domain.com" || !Hash(password).Equals(Hash("password")))
+            {
+                return null;
+            }
+            User user = new User() { Id = 1, Email = "email@domain.com", PasswordHash = Hash("password"), Name = "Ole Petter Dahlmann" };
+            
+            return user;
+        }
+    }
+}
